92efd4278e9f89746f3799be9ababc6fd1ec3ddd
Papanister <pporkins7@gmail.com>
2025-07-25 13:01:41 +0300

    Update PIECES 2.cs

--- 01 Идеи проектов/Целевые проекты/Upper Level/PIECES 2.cs
+++ 01 Идеи проектов/Целевые проекты/Upper Level/PIECES 2.cs
@@ -36,3 +36,20 @@ public static int StringIntersection(string? str1, string? str2) {
 
     return dp[len1, len2];
 }
+
+/// xUnit testing method
+[
+    Theory,
+    InlineData("", "", 0),
+    InlineData(null, null, 0),
+    InlineData(null, "", int.MaxValue),
+    InlineData("TEXT_TEXT", "TEXT_TEX", 1),
+    InlineData("TEXT_text", "TEXT_TEXT", 4),
+    InlineData("TEXT_text", "TEXT_", 4),
+    InlineData("TEXT_TEXT", "TEXTTEXT", 1),
+    InlineData("TEXT_TEXT", "TEXTtext", 5),
+]
+public void StringIntersectionTest(string? str1, string str2, int expected) {
+    var actual =  API.StringIntersection(str1, str2);
+    Assert.Equal(expected, actual);
+}
